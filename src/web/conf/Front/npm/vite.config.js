b1faa73d365c8eb26b299747d7553512e82c6830
root <root@osboxes>
2024-09-27 16:32:51 +0200

    micro service setup + reorga

---
+++ src/web/conf/Front/npm/vite.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite'
+import path from 'path'
+
+export default defineConfig({
+    server: {
+        host: '0.0.0.0',
+        port: 8080,
+    },
+    resolve: {
+        alias: [
+            { find: '@js', replacement: path.resolve(__dirname, 'src/js/') },
+            { find: '@css', replacement: path.resolve(__dirname, 'src/css/') },
+        ],
+      },    
+})
+
