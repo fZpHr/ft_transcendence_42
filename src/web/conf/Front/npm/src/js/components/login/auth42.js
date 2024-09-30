fcfdf0812b44b5be4f165b6b13a978db816f7683
fZpHr <afterrush@live.fr>
2024-09-30 22:48:12 +0200

    login global, vm && extern

--- src/web/conf/Front/npm/src/js/components/login/auth42.js
+++ src/web/conf/Front/npm/src/js/components/login/auth42.js
@@ -28,10 +28,13 @@ export class Auth42 extends Component{
         console.log("Autre truc added to page.");
         const loginButton = this.querySelector("#loginButton");
         loginButton.addEventListener("click", () => {
-            const redirectUri = encodeURIComponent('https://localhost/users/register-42/');
-
-            const authUrl = `https://api.intra.42.fr/oauth/authorize?client_id=u-s4t2ud-74438314e8cff2be68aee7a119f4c95bff6ba35b11a2bf5c2627a31a869c9f28&redirect_uri=${redirectUri}&response_type=code`;
-            
+            let authUrl;
+            if (window.origin.includes('42424')) {
+                authUrl = `https://api.intra.42.fr/oauth/authorize?client_id=u-s4t2ud-74438314e8cff2be68aee7a119f4c95bff6ba35b11a2bf5c2627a31a869c9f28&redirect_uri=https%3A%2F%2Flocalhost%3A42424%2Fusers%2Fregister-42%2F&response_type=code`;
+            } else {
+                authUrl = `https://api.intra.42.fr/oauth/authorize?client_id=u-s4t2ud-74438314e8cff2be68aee7a119f4c95bff6ba35b11a2bf5c2627a31a869c9f28&redirect_uri=https%3A%2F%2Flocalhost%2Fusers%2Fregister-42%2F&response_type=code`;
+            }
+    
             window.location.href = authUrl;
         });
     }
--- src/web/conf/UserManagement/conf/Register42/views.py
+++ src/web/conf/UserManagement/conf/Register42/views.py
@@ -6,32 +6,38 @@ import logging
 
 logger = logging.getLogger('print')
 
-def register_42(request):
-    hostname = request.get_host()
-    redirect_uri = f"https://{hostname}/users/register-42/"
-    
-    code = request.GET.get("code")
-    if code is None:
-        logger.info(f'Redirecting to 42 OAuth2')
-        return HttpResponseRedirect(f"https://api.intra.42.fr/oauth/authorize?client_id={os.getenv('client_id')}&redirect_uri={redirect_uri}&response_type=code")
-    logger.info(f'code: {code}')
-    logger.info(f'redirect_uri: {redirect_uri}')
+
+def get_token(code, redirect_uri):
     body = {
         "grant_type": "authorization_code",
         "client_id": os.getenv("client_id"),
         "client_secret": os.getenv("client_secret"),
         "code": code,
         "redirect_uri": redirect_uri
-    }
-    logger.info(f'body: {body}')
+    }  
     headers = {"Content-Type": "application/json; charset=utf-8"}
     r = requests.post('https://api.intra.42.fr/oauth/token', headers=headers, json=body)
-    
-    if r.status_code != 200:
-        logger.error(f'Failed to get access token: {r.status_code} {r.text}')
-        return JsonResponse({"error": "Failed to get access token"}, status=400)
-    
-    response_json = r.json()
+    return r
+
+
+def register_42(request):
+    extern = 0
+    redirect_uri = "https://localhost/users/register-42/"
+    code = request.GET.get("code")
+    if code is None:
+        logger.info(f'Redirecting to 42 OAuth2')
+        return HttpResponseRedirect(f"https://api.intra.42.fr/oauth/authorize?client_id={os.getenv('client_id')}&redirect_uri={redirect_uri}&response_type=code")
+
+
+    response = get_token(code, redirect_uri)
+    if response.status_code != 200:
+        redirect_uri = "https://localhost:42424/users/register-42/"
+        extern = 1
+        response = get_token(code, redirect_uri)
+        if response.status_code != 200:
+            return JsonResponse({"error": f"Failed to get access token {redirect_uri}"}, status=400)
+
+    response_json = response.json()
     if "access_token" not in response_json:
         logger.error(f'Access token not found in response: {response_json}')
         return JsonResponse({"error": "Access token not found"}, status=400)
@@ -43,8 +49,10 @@ def register_42(request):
     if r.status_code != 200:
         logger.error(f'Failed to get user info: {r.status_code} {r.text}')
         return JsonResponse({"error": "Invalid access token"}, status=400)
-
-    response = redirect('https://localhost/')
+    if (extern):
+        response = redirect('https://localhost:42424/')
+    else:
+        response = redirect('https://localhost/')
     
     response.set_cookie('token_user42', token, httponly=True)
     
